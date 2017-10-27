469ddc30b42b5d365d6d1c7f68fead29c730f233
leirelitw <llitwinechevarria@mail.sfsu.edu>
2017-10-26 17:26:08 -0700

    milestone2
    milestone2

--- roulette/my-app/app.js
+++ roulette/my-app/app.js
@@ -1,7 +1,3 @@
-if (process.env.NODE_ENV === 'development') {
-   require('dotenv').config();
-}
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -11,8 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var tests = require('./routes/tests');
-
 
 var app = express();
 
@@ -24,9 +18,7 @@ app.set('view engine', 'jade');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-   extended: false
-}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -35,20 +27,20 @@ app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-   var err = new Error('Not Found');
-   err.status = 404;
-   next(err);
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-   // set locals, only providing error in development
-   res.locals.message = err.message;
-   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-   // render the error page
-   res.status(err.status || 500);
-   res.render('error');
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
