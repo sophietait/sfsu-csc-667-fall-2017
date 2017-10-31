c76a7e5a4452d9b48e9470874c86c50ad9c64869
Leire Litwin <leirelitwin@sfs-wifi-dhcp-10-142-14-161.wifi.sfsu.edu>
2017-10-31 15:54:15 -0700

    ,
    ,

--- roulette/routes/tests.js
+++ roulette/routes/tests.js
@@ -1,15 +1,15 @@
 var express = require("express");
 var router = express.Router();
-
 var db = require('../db');
-router.get("/", (request, response) => {
-    db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at ${Date.now()}')`)
-        .then( _ => db.any(`SELECT * FROM test_table`) )
-        .then( results => response.json( results ) )
-        .catch( error => {
-            console.log( error );
-            response.json({ error })
-        })
+
+router.get('/', function(req, res, next) {
+  //res.send('respond with a resource');
+  db.any(`INSERT INTO test_table ("testString") VALUES ('Hello at ${Date.now()}')`)
+.then( _ => db.any(`SELECT * FROM test_table`) )
+.then( results => res.json( results ) )
+.catch( error => {
+console.log( error )
+res.json({ error })
 });
-//hello
+});
 module.exports = router;
