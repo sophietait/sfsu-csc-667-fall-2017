84f3832e147f4aa9e997531919b559e1cc258dd8
Leire Litwin <leirelitwin@sfs-wifi-dhcp-10-142-14-201.wifi.sfsu.edu>
2017-11-14 18:48:31 -0800

    login
    login

--- roulette/db/index.js
+++
@@ -1,4 +0,0 @@
-const pgp = require('pg-promise')();
-require('dotenv').config();
-const connection = pgp(process.env.DATABASE_URL);
-module.exports = connection;
--- roulette/game/games.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(request, response, next)) {
-response.render('games/home');
-//res.send('hoaljds');
-});
-//router.get('/all', (request, response))
