d3818243d00194bcfc40ae78cd401ca4a28f3442
leire <llitwinechevarria@mail.sfsu.edu>
2017-12-05 20:24:57 -0800

    get to post
    get to post

--- routes/signup.js
+++ routes/signup.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('signup', { title: 'Sign up' });
 });
 
+router.post('/saveSignUpData', function(req, res, next) {
+  db.any(`INSERT INTO player ("username", "password", "email_id") VALUES (`+user+`,`+pass+`,`+email+`)`)
+.then( _ => db.any(`SELECT * FROM game`) )
+.then( results => res.json( results ) )
+.catch( error => {
+console.log( error )
+res.json({ error })
+});
+});
+
 module.exports = router;
