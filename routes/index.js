e0e6bbcb342f44d414d0a8404f8aec34493c9b41
leire <llitwinechevarria@mail.sfsu.edu>
2017-12-06 14:50:28 -0800

    signup
    signup

--- routes/index.js
+++ routes/index.js
@@ -9,20 +9,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Roulette' });
 });
 
-/*
-router.get('/gamelob', function(req, res, next) {
-  //db.any(`SELECT ("username", "password") FROM player`)
-  db.any(`SELECT username, password FROM player`)
-
-//.then( _ => db.any(`SELECT * FROM test_table`) )
-.then( results => res.json( results ) )
-.catch( error => {
-console.log( error )
-res.json({ error })
-});
-});
-*/
-
 passport.use(new LocalStrategy(function(username, password, done) {
   User.findOne({ username: username }, function(err, user) {
     if (err) { return done(err); }
@@ -38,32 +24,12 @@ passport.use(new LocalStrategy(function(username, password, done) {
 ));
 
 
-app.post('/gamelobby',
-passport.authenticate('local', { successRedirect: '/gamelobby',
-                                 failureRedirect: '/',
-                                 failureFlash: true })
+router.post('/',passport.authenticate('local-index', { successRedirect: '/gamelobby',
+                                 failureRedirect: '/index',
+                                 failureFlash: true }),
+                                 function(req, res, next) {
+                                  res.render('index', { title: 'Roulette' });
+                                }                                 
 );
 
-
-
-
 module.exports = router;
-
-/*
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-
-passport.use(new LocalStrategy(function(username, password, done) {
-  User.findOne({ username: username }, function(err, user) {
-    if (err) { return done(err); }
-    if (!user) {
-      return done(null, false, { message: 'Incorrect username.' });
-    }
-    if (!user.validPassword(password)) {
-      return done(null, false, { message: 'Incorrect password.' });
-    }
-    return done(null, user);
-  });
-}
-));
-*/
--- routes/signup.js
+++ routes/signup.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../db/index.js');
+var player_id = 4;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('signup', { title: 'Sign up' });
+});
+
+router.post('/', function(req, res, next) {
+  console.log(req.body.username);
+  player_id+=1;
+  db.any("INSERT INTO player (player_id, username, email_id, password, balance, state) VALUES ('"+player_id+"','"+req.body.username+"','"+req.body.email+"','"+req.body.password+"',100,0);")
+  res.render('index', { title: 'Roulette' });
 });
 
 module.exports = router;
